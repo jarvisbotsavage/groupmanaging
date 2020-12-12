c4348c97f3d1ab9c8354b1696bf202a5f7cd3063
jarvisbotsavage <73605853+jarvisbotsavage@users.noreply.github.com>
2020-12-12 21:29:17 +0530

    Update __main__.py

--- haruka/__main__.py
+++ haruka/__main__.py
@@ -26,6 +26,8 @@ You know how hard it is sometimes to manage group so here is the solution for yo
 
 I'm group manager bot maded by peeyush developer
 
+Haters stay away
+
 Activate Anti-spam protection in your BM by this command : 
 /antispam on 
 
@@ -39,7 +41,7 @@ Watch [UPDATED SOON TUTORIAL](https://www.youtube.com/watch?v=gXXFp) on deployin
 
 Click /help or Help button below to find out more about how to use me to my full potential.
 
- Add me to a group by clicking [here](http://t.me/tg_GroupManagerBot?startgroup=true).
+ Add me to a group by clicking [here](http://t.me/Peeyushop_bot?startgroup=true).
 """
 
 
